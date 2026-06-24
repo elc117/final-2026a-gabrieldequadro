f306202cbc3f21dbebc0c86b45ad336bc3436421
GabrieldeQuadro <gabriel.schutz@acad.ufsm.br>
2026-06-23 22:21:22 -0300

    Mundanças no mapa adição sprites

--- lwjgl3/build/resources/main/Assets.tsx
+++ lwjgl3/build/resources/main/Assets.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.12.2" name="Assets" tilewidth="16" tileheight="16" tilecount="322" columns="23">
-    <image source="Assets.png" width="400" height="256"/>
+ <image source="Assets.png" width="368" height="224"/>
 </tileset>
